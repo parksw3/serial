02a79f347ac23f54b819c92b967b7093eb64adb9
parksw3 <parksw3@mcmaster.ca>
2020-04-28 20:43:53 -0400

    pushing

--- serial_guide.R
+++ serial_guide.R
@@ -17,13 +17,54 @@ g1 <- ggplot(arrow) +
   geom_point(aes(0.5, 0.2), size=3) +
   geom_point(aes(1.5, 0.4), size=3) +
   geom_point(aes(2.5, 0.3), size=3) +
-  annotate("text", x=1.5, y=-0.05, label="$\\pi$") +
-  annotate("text", x=0.5, y=-0.05, label="$\\pi-x_0$") +
+  geom_point(aes(0.5, 0.1), size=3) +
+  annotate("text", x=1.5, y=-0.05, label="$p+x_0$") +
+  annotate("text", x=0.5, y=-0.05, label="$p$") +
+  annotate("text", x=0.5, y=-0.1, label="Infector") +
+  annotate("text", x=2.5, y=-0.1, label="Infectee") +
+  annotate("text", x=2.5, y=-0.05, label="$p+\\sigma$") +
+  annotate("text", x=3.5, y=-0.05, label="$p+\\sigma+x_1$") +
+  geom_segment(aes(x, y, xend=x1, yend=y1), arrow = arrow(length = unit(0.2, "inches")), lwd=1) +
+  geom_segment(aes(0.5, 0, xend=0.5, yend=0.2), lty=2) +
+  geom_segment(aes(1.5, 0, xend=1.5, yend=0.4), lty=2) +
+  geom_segment(aes(2.5, 0, xend=2.5, yend=0.3), lty=2) +
+  geom_segment(aes(3.5, 0, xend=3.5, yend=0.4), lty=2) +
+  geom_segment(aes(0.5, 0.1, xend=1.5, yend=0.1), arrow = arrow(length = unit(0.1, "inches")), lwd=1) +
+  geom_segment(aes(0.5, 0.2, xend=2.5, yend=0.2), arrow = arrow(length = unit(0.1, "inches")), lwd=1) +
+  geom_segment(aes(2.5, 0.3, xend=3.5, yend=0.3), arrow = arrow(length = unit(0.1, "inches")), lwd=1) +
+  geom_segment(aes(1.5, 0.4, xend=3.5, yend=0.4), arrow = arrow(length = unit(0.1, "inches")), lwd=1) +
+  annotate("text", x=2.5, y=0.45, label="Intrinsic serial interval") +
+  annotate("text", x=1, y=0.15, label="Forward incubation period") +
+  annotate("text", x=1, y=0.25, label="Forward generation interval") +
+  annotate("text", x=3, y=0.35, label="Forward incubation period") +
+  geom_point(aes(1.5, 0), size=4, shape=21, fill="white", stroke=2) +
+  geom_point(aes(3.5, 0), size=4, shape=21, fill="black", stroke=2) +
+  scale_y_continuous(limits=c(-0.15, 0.45)) +
+  ggtitle("A. Intrinsic serial interval") +
+  theme(
+    panel.grid = element_blank(),
+    panel.border = element_blank(),
+    axis.ticks = element_blank(),
+    axis.text = element_blank(),
+    axis.title = element_blank()
+  )
+
+g2 <- ggplot(arrow) +
+  geom_point(aes(0.5, 0), size=3) +
+  geom_point(aes(1.5, 0), size=3) +
+  geom_point(aes(2.5, 0), size=3) +
+  geom_point(aes(3.5, 0), size=3) +
+  geom_point(aes(0.5, 0.2), size=3) +
+  geom_point(aes(1.5, 0.4), size=3) +
+  geom_point(aes(2.5, 0.3), size=3) +
+  geom_point(aes(1.5, 0.1), size=3) +
+  annotate("text", x=1.5, y=-0.05, label="$p$") +
+  annotate("text", x=0.5, y=-0.05, label="$p-x_0$") +
   annotate("text", x=0.5, y=-0.1, label="Infector") +
   annotate("text", x=1.5, y=-0.1, label="Primary cohort") +
   annotate("text", x=2.5, y=-0.1, label="Infectee") +
-  annotate("text", x=2.5, y=-0.05, label="$\\pi-x_0+\\sigma$") +
-  annotate("text", x=3.5, y=-0.05, label="$\\pi-x_0+\\sigma+x_1$") +
+  annotate("text", x=2.5, y=-0.05, label="$p-x_0+\\sigma$") +
+  annotate("text", x=3.5, y=-0.05, label="$p-x_0+\\sigma+x_1$") +
   geom_segment(aes(x, y, xend=x1, yend=y1), arrow = arrow(length = unit(0.2, "inches")), lwd=1) +
   geom_segment(aes(0.5, 0, xend=0.5, yend=0.2), lty=2) +
   geom_segment(aes(1.5, 0, xend=1.5, yend=0.4), lty=2) +
@@ -40,7 +81,7 @@ g1 <- ggplot(arrow) +
   geom_point(aes(1.5, 0), size=4, shape=21, fill="white", stroke=2) +
   geom_point(aes(3.5, 0), size=4, shape=21, fill="black", stroke=2) +
   scale_y_continuous(limits=c(-0.15, 0.45)) +
-  ggtitle("A") +
+  ggtitle("B. Forward serial interval") +
   theme(
     panel.grid = element_blank(),
     panel.border = element_blank(),
@@ -49,18 +90,19 @@ g1 <- ggplot(arrow) +
     axis.title = element_blank()
   )
 
-g2 <- ggplot(arrow) +
+g3 <- ggplot(arrow) +
   geom_point(aes(0.5, 0), size=3) +
   geom_point(aes(1.5, 0), size=3) +
   geom_point(aes(2.5, 0), size=3) +
   geom_point(aes(3.5, 0), size=3) +
+  geom_point(aes(3.5, 0.1), size=3) +
   geom_point(aes(0.5, 0.3), size=3) +
   geom_point(aes(3.5, 0.4), size=3) +
   geom_point(aes(2.5, 0.2), size=3) +
-  annotate("text", x=3.5, y=-0.05, label="$\\delta$") +
-  annotate("text", x=2.5, y=-0.05, label="$\\delta-x_1$") +
-  annotate("text", x=0.5, y=-0.05, label="$\\delta-x_1-\\sigma$") +
-  annotate("text", x=1.5, y=-0.05, label="$\\delta-x_1-\\sigma+x_0$") +
+  annotate("text", x=3.5, y=-0.05, label="$s$") +
+  annotate("text", x=2.5, y=-0.05, label="$s-x_1$") +
+  annotate("text", x=0.5, y=-0.05, label="$s-x_1-\\sigma$") +
+  annotate("text", x=1.5, y=-0.05, label="$s-x_1-\\sigma+x_0$") +
   annotate("text", x=0.5, y=-0.1, label="Infector") +
   annotate("text", x=2.5, y=-0.1, label="Infectee") +
   annotate("text", x=3.5, y=-0.1, label="Secondary cohort") +
@@ -80,7 +122,7 @@ g2 <- ggplot(arrow) +
   geom_point(aes(3.5, 0), size=4, shape=21, fill="white", stroke=2) +
   geom_point(aes(1.5, 0), size=4, shape=21, fill="black", stroke=2) +
   scale_y_continuous(limits=c(-0.15, 0.45)) +
-  ggtitle("B") +
+  ggtitle("C. Backward serial interval") +
   theme(
     panel.grid = element_blank(),
     panel.border = element_blank(),
@@ -89,7 +131,7 @@ g2 <- ggplot(arrow) +
     axis.title = element_blank()
   )
 
-tikz(file = "serial_guide.tex", width = 10, height = 6, standAlone = T)
-grid.arrange(g1, g2, nrow=2)
+tikz(file = "serial_guide.tex", width = 10, height = 8, standAlone = T)
+grid.arrange(g1, g2, g3, nrow=3)
 dev.off()
 tools::texi2dvi('serial_guide.tex', pdf = T, clean = T)
