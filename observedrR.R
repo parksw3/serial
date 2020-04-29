168401848d6ad27cc80238e9a79dcef6b166576d
parksw3 <parksw3@mcmaster.ca>
2020-04-29 11:32:50 -0400

    almost screwed up...

--- observedrR.R
+++ observedrR.R
@@ -1,8 +1,9 @@
 library(tidyr)
 library(dplyr)
-library(ggplot2); theme_set(theme_bw())
+library(ggplot2); theme_set(theme_bw(base_size = 16))
 library(gridExtra)
 library(tikzDevice)
+source("rRfun.R")
 source("renewal_det.R")
 source("cpalette.R")
 
@@ -111,42 +112,103 @@ R0_cohort <- lapply(tcut, function(x) {
 ser_all <- bind_rows(ser_obs, ser_cohort) %>%
   mutate(
     type=factor(type, level=c("Observed", "Cohort-averaged"))
+  ) %>%
+  group_by(type, t) %>%
+  summarize(
+    mean=mean(ser),
+    lwr=min(ser),
+    upr=max(ser)
   )
 
-g1 <- ggplot(ser_all) +
-  geom_line(aes(t, ser, col=type, group=interaction(sim, type), lty=type)) +
+serialsim <- serialR2(r=r, rho=0) 
+
+g1a <- ggplot(filter(ser_all, type=="Observed")) +
+  geom_ribbon(aes(t, ymin=lwr, ymax=upr, fill=type, col=type, lty=type), alpha=0.5) +
+  geom_line(aes(t, mean, col=type, lty=type)) +
   geom_hline(yintercept=rr$mfser2[1], lty=2) +
+  geom_hline(yintercept=serialsim$mean, lty=1) +
+  annotate("text", x=78, y=rr$mfser2[1], label="Initial forward", hjust=1, vjust=-0.3, size=5) +
+  annotate("text", x=78, y=serialsim$mean, label="Intrinsic", hjust=1, vjust=-0.3, size=5) +
   scale_x_continuous("Time (days)", limits=c(0, 80), expand=c(0, 0)) +
-  scale_y_continuous("Mean serial interval (days)", expand=c(0, 0), limits=c(2, 7.2)) +
+  scale_y_continuous("Mean serial interval (days)", expand=c(0, 0), limits=c(2, 6.3)) +
   scale_color_manual(values=tail(cpalette, 2)) +
   scale_fill_manual(values=tail(cpalette, 2)) +
-  ggtitle("A") +
-  theme(
-    panel.grid = element_blank(),
-    legend.position = c(0.75, 0.2),
-    legend.title = element_blank()
-  )
-
-R0all <- bind_rows(R0_obs, R0_cohort) %>%
-  mutate(
-    type=factor(type, level=c("Observed", "Cohort-averaged"))
-  )
-
-g2 <- ggplot(R0all) +
-  geom_line(aes(t, R0, col=type, group=interaction(sim, type), lty=type)) +
-  geom_hline(yintercept=rr$R0, lty=2) +
-  scale_x_continuous("Time (days)", limits=c(0, 80), expand=c(0, 0)) +
-  scale_y_continuous("Reproduction number $\\mathcal R$", expand=c(0, 0), limits=c(1.3, 3.3)) +
-  scale_color_manual(values=tail(cpalette, 2)) +
-  scale_fill_manual(values=tail(cpalette, 2)) +
-  ggtitle("B") +
   theme(
     panel.grid = element_blank(),
     legend.position = "none",
     legend.title = element_blank()
   )
 
-tikz(file = "observedrR.tex", width = 8, height = 3, standAlone = T)
-grid.arrange(g1, g2, nrow=1)
+g1b <- ggplot(filter(ser_all, type=="Cohort-averaged")) +
+  geom_ribbon(aes(t, ymin=lwr, ymax=upr, fill=type, col=type, lty=type), alpha=0.5) +
+  geom_line(aes(t, mean, col=type, lty=type)) +
+  geom_hline(yintercept=rr$mfser2[1], lty=2) +
+  geom_hline(yintercept=serialsim$mean, lty=1) +
+  annotate("text", x=78, y=rr$mfser2[1], label="Initial forward", hjust=1, vjust=-0.3, size=5) +
+  annotate("text", x=78, y=serialsim$mean, label="Intrinsic", hjust=1, vjust=-0.3, size=5) +
+  scale_x_continuous("Time (days)", limits=c(0, 80), expand=c(0, 0)) +
+  scale_y_continuous("Mean serial interval (days)", expand=c(0, 0), limits=c(4.5, 7.2)) +
+  scale_color_manual(values=tail(cpalette, 1)) +
+  scale_fill_manual(values=tail(cpalette, 1)) +
+  theme(
+    panel.grid = element_blank(),
+    legend.position = "none",
+    legend.title = element_blank()
+  )
+
+R0all <- bind_rows(R0_obs, R0_cohort) %>%
+  mutate(
+    type=factor(type, level=c("Observed", "Cohort-averaged"))
+  ) %>%
+  group_by(type, t) %>%
+  summarize(
+    mean=mean(R0),
+    lwr=min(R0),
+    upr=max(R0)
+  )
+
+g2a <- ggplot(filter(R0all, type=="Observed")) +
+  geom_ribbon(aes(t, ymin=lwr, ymax=upr, fill=type, col=type, lty=type), alpha=0.5) +
+  geom_line(aes(t, mean, col=type, lty=type)) +
+  geom_hline(yintercept=rr$R0, lty=2) +
+  geom_hline(yintercept=serialsim$R, lty=1) +
+  annotate("text", x=78, y=rr$R0, label="Initial forward", hjust=1, vjust=-0.3, size=5) +
+  annotate("text", x=78, y=serialsim$R, label="Intrinsic", hjust=1, vjust=-0.3, size=5) +
+  scale_x_continuous("Time (days)", limits=c(0, 80), expand=c(0, 0)) +
+  scale_y_continuous("Reproduction number $\\mathcal R$", expand=c(0, 0), limits=c(0.9, 2.7),
+                     breaks=c(1, 2, 3)) +
+  scale_color_manual(values=tail(cpalette, 2)) +
+  scale_fill_manual(values=tail(cpalette, 2)) +
+  theme(
+    panel.grid = element_blank(),
+    legend.position = "none",
+    legend.title = element_blank()
+  )
+
+g2b <- ggplot(filter(R0all, type=="Cohort-averaged")) +
+  geom_ribbon(aes(t, ymin=lwr, ymax=upr, fill=type, col=type, lty=type), alpha=0.5) +
+  geom_line(aes(t, mean, col=type, lty=type)) +
+  geom_hline(yintercept=rr$R0, lty=2) +
+  geom_hline(yintercept=serialsim$R, lty=1) +
+  annotate("text", x=78, y=rr$R0, label="Initial forward", hjust=1, vjust=-0.3, size=5) +
+  annotate("text", x=78, y=serialsim$R, label="Intrinsic", hjust=1, vjust=-0.3, size=5) +
+  scale_x_continuous("Time (days)", limits=c(0, 80), expand=c(0, 0)) +
+  scale_y_continuous("Reproduction number $\\mathcal R$", expand=c(0, 0), limits=c(1.3, 3.3),
+                     breaks=c(1, 2, 3)) +
+  scale_color_manual(values=tail(cpalette, 1)) +
+  scale_fill_manual(values=tail(cpalette, 1)) +
+  theme(
+    panel.grid = element_blank(),
+    legend.position = "none",
+    legend.title = element_blank()
+  )
+
+tikz(file = "observedrRa.tex", width = 6.6, height = 3.3, standAlone = T)
+grid.arrange(g1a, g2a, nrow=1)
 dev.off()
-tools::texi2dvi('observedrR.tex', pdf = T, clean = T)
+tools::texi2dvi('observedrRa.tex', pdf = T, clean = T)
+
+tikz(file = "observedrRb.tex", width = 6.6, height = 3.3, standAlone = T)
+grid.arrange(g1b, g2b, nrow=1)
+dev.off()
+tools::texi2dvi('observedrRb.tex', pdf = T, clean = T)
--- rR.R
+++ rR.R
@@ -3,6 +3,7 @@ library(ggplot2); theme_set(theme_bw())
 library(tikzDevice)
 library(gridExtra)
 source("cpalette.R")
+source("rRfun.R")
 
 ## incubation!
 meanlog1 <- 1.62
@@ -13,80 +14,7 @@ sd(rlnorm(50000, meanlog = 1.62, sdlog=0.42)) ## 2.4
 ## generation
 ## moment matching
 mean(rlnorm(50000, meanlog = 1.54, sdlog=0.37)) ## 5 days
-sd(rlnorm(50000, meanlog = 1.54, sdlog=0.37)) ##
-
-serialR <- function(r=0.1,
-                    rho=0.5,
-                    meanlog1=1.62,
-                    sdlog1=0.42,
-                    meanlog2=1.54,
-                    sdlog2=0.37,
-                    nsim=100000,
-                    seed=101) {
-  set.seed(seed)
-  backinc <- rlnorm(nsim, meanlog=meanlog1, sdlog=sdlog1)
-  
-  backinc <- sample(backinc, replace=TRUE, prob=exp(-r*backinc))
-  
-  logbackinc <- log(backinc)
-  
-  muloggen <- meanlog2 + sdlog2 * rho * (logbackinc - meanlog1)/sdlog1
-  sigmaloggen <- sqrt(sdlog2^2 * (1 - rho^2))
-  
-  forwardgen <- exp(rnorm(nsim, muloggen, sigmaloggen))
-  forwardinc <- rlnorm(nsim, meanlog=meanlog1, sdlog=sdlog1)
-  
-  ser <- -backinc + forwardgen + forwardinc
-  
-  list(
-    mean=mean(ser),
-    kappa=var(ser)/mean(ser)^2,
-    R=1/mean(exp(-r*ser))
-  )
-}
-
-serialR2 <- function(r=0.1,
-                    rho=0.5,
-                    meanlog1=1.62,
-                    sdlog1=0.42,
-                    meanlog2=1.54,
-                    sdlog2=0.37,
-                    nsim=100000,
-                    seed=101) {
-  set.seed(seed)
-  backinc <- rlnorm(nsim, meanlog=meanlog1, sdlog=sdlog1)
-  
-  logbackinc <- log(backinc)
-  
-  muloggen <- meanlog2 + sdlog2 * rho * (logbackinc - meanlog1)/sdlog1
-  sigmaloggen <- sqrt(sdlog2^2 * (1 - rho^2))
-  
-  forwardgen <- exp(rnorm(nsim, muloggen, sigmaloggen))
-  forwardinc <- rlnorm(nsim, meanlog=meanlog1, sdlog=sdlog1)
-  
-  ser <- -backinc + forwardgen + forwardinc
-  
-  list(
-    mean=mean(ser),
-    kappa=var(ser)/mean(ser)^2,
-    R=1/mean(exp(-r*ser))
-  )
-}
-
-generationR <- function(r=0.1,
-                        meanlog2=1.54,
-                        sdlog2=0.37,
-                        nsim=100000,
-                        seed=101) {
-  set.seed(101)
-  gen <- rlnorm(nsim, meanlog2, sdlog2)
-                          
-  list(
-    mean=mean(gen),
-    kappa=var(gen)/mean(gen)^2,
-    R=1/mean(exp(-r*gen))
-  )
-}
+sd(rlnorm(50000, meanlog = 1.54, sdlog=0.37)) ## 1.9 days
 
 ww <- c(1, 5, 9, 13, 17, 21, 25, 29)
 
