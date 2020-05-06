ff7636c34fe14f2282eb3bdbad00839dd03092c6
parksw3 <parksw3@mcmaster.ca>
2020-05-05 21:09:01 -0400

    some tweaks

--- serial_guide.R
+++ serial_guide.R
@@ -34,9 +34,9 @@ g1 <- ggplot(arrow) +
   geom_segment(aes(2.5, 0.3, xend=3.5, yend=0.3), arrow = arrow(length = unit(0.1, "inches")), lwd=1) +
   geom_segment(aes(1.5, 0.4, xend=3.5, yend=0.4), arrow = arrow(length = unit(0.1, "inches")), lwd=1) +
   annotate("text", x=2.5, y=0.45, label="Intrinsic serial interval") +
-  annotate("text", x=1, y=0.15, label="Forward incubation period") +
-  annotate("text", x=1, y=0.25, label="Forward generation interval") +
-  annotate("text", x=3, y=0.35, label="Forward incubation period") +
+  annotate("text", x=1, y=0.15, label="Intrinsic incubation period") +
+  annotate("text", x=1, y=0.25, label="Intrinsic generation interval") +
+  annotate("text", x=3, y=0.35, label="Intrinsic incubation period") +
   geom_point(aes(0.5, 0), size=4, shape=21, fill="white", stroke=2) +
   scale_y_continuous(limits=c(-0.15, 0.45)) +
   ggtitle("A. Intrinsic serial interval") +
