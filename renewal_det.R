ab06da822c3d70caef2aa714b464bccb5fa6e988
parksw3 <parksw3@mcmaster.ca>
2020-05-10 20:46:37 -0400

    small changes to the code

--- renewal_det.R
+++ renewal_det.R
@@ -12,7 +12,7 @@ renewal_det <- function(R0=2.5,
   gen <- genfun(0:genmax*dt+dt)
   gen <- gen/sum(gen)
   tvec <- seq(0, tmax, by=dt)
-  Ivec <- rep(0, dt*tmax)
+  Ivec <- rep(0, tmax/dt)
   
   r <- optim(0.19, function(x) (1/sum(exp(-x*(0:genmax*dt+dt)) * gen)-R0)^2,
         lower=0.1,
@@ -22,8 +22,8 @@ renewal_det <- function(R0=2.5,
   ## r=0.1904075 for R0=2.5
   Ivec[1:genmax] <- exp(r*1:genmax*dt)
   Ivec[1:genmax] <- I0*Ivec[1:genmax]/sum(Ivec[1:genmax])
-  Svec <- rep(0, dt*tmax)
-  Svec[1:genmax] <- N - cumsum(Ivec)
+  Svec <- rep(0, tmax/dt)
+  Svec[1:genmax] <- N - cumsum(Ivec)[1:genmax]
   
   for (i in (genmax+1):length(tvec)) {
     Ivec[i] <- Svec[i-1] * R0 * sum(Ivec[max(1, i-genmax):(i-1)] * gen[min(i, genmax+1):2])/N
